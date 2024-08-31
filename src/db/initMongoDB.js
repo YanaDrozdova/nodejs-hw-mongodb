07ef54062248e80c017b472a7440555876fd627c
YanaDrozdova <yadro986@gmail.com>
2024-08-31 16:12:03 +0300

    added db & routes

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -9,7 +9,9 @@ export const initMongoDB = async () => {
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
     const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+
     await mongoose.connect(DB_HOST);
+
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.log('Mongo connection error', error.message);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import { env } from './utils/env.js';
 
-// const port = Number(process.env.PORT) || 3000;
+import { env } from './utils/env.js';
+import * as contactServices from './services/contacts.js';
 
 export const setupServer = () => {
   const app = express();
@@ -18,6 +18,32 @@ export const setupServer = () => {
   app.use(express.json());
 
   //routes
+  app.get('/contacts', async (req, res) => {
+    // const data = await ContactCollection.find();
+    const data = await contactServices.getAllContacts();
+
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts',
+      data,
+    });
+  });
+
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.getContactById(id);
+
+    if (!data) {
+      return res.status(404).json({
+        message: `Contact with id=${id} not found`,
+      });
+    }
+    res.json({
+      status: 200,
+      message: `Contact with ID: ${id} successfully found`,
+      data,
+    });
+  });
 
   app.use((req, res) => {
     res.status(404).json({
