82055580ce4aeadd0632fc6a43940d7eb23b35f9
YanaDrozdova <yadro986@gmail.com>
2024-09-13 17:55:38 +0300

    added validation ID and joi schema

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,17 +7,32 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import validateBody from '../utils/validateBody.js';
+import {
+  contactsAddSchema,
+  contactsUpdateSchema,
+} from '../validation/contacts.js';
+import isValidId from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getAllContactsController));
 
-router.get('/:id', ctrlWrapper(getContactByIdController));
+router.get('/:id', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post(
+  '/',
+  validateBody(contactsAddSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.patch('/:id', ctrlWrapper(patchContactController));
+router.patch(
+  '/:id',
+  isValidId,
+  validateBody(contactsUpdateSchema),
+  ctrlWrapper(patchContactController),
+);
 
-router.delete('/:id', ctrlWrapper(deleteContactController));
+router.delete('/:id', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
