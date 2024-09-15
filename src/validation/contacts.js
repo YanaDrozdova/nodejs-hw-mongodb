db71b31b9cf3a1091eef6201c86dd3caf33bc3a8
YanaDrozdova <yadro986@gmail.com>
2024-09-15 12:42:28 +0300

    update validation/contacts

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,7 +6,7 @@ export const contactsAddSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20).email(),
-  isFavourite: Joi.boolean().required(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .valid(...contactTypeList)
     .required(),
